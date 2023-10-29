6bdf78170a9d624888f9c65e155aaf6e0b218e19
Yessen Murat <shoyo2209@gmail.com>
2023-10-30 00:07:22 +0600

    add additional columns to table and models

--- src/models.py
+++ src/models.py
@@ -1,21 +1,26 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 
 
 class Tournament(Base):
-    __tablename__ = 'tournament'
+    __tablename__ = "tournament"
 
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String, nullable=False)
     image = Column(String, nullable=False)
-    time = Column(String, nullable=False)
+    start_date = Column(String, nullable=False)
+    end_date = Column(String, nullable=False)
     location = Column(String, nullable=False)
+    city = Column(String, nullable=False)
     league = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
     main_referee = Column(String, nullable=False)
     level = Column(String, nullable=False)
     language = Column(String, nullable=False)
     link = Column(String, nullable=False)
+    sponsor_link = Column(String, nullable=False)
     format = Column(String, nullable=False)
     fraction = Column(String, nullable=False)
     referee = Column(String, nullable=False)
+    prize_pool = Column(Integer, nullable=False)
+    volume = Column(Float, nullable=False)
--- src/schemas.py
+++ src/schemas.py
@@ -1,19 +1,25 @@
 from pydantic import BaseModel
 
+
 class BaseTournament(BaseModel):
     name: str
     image: str
-    time: str
+    start_date: str
+    end_date: str
     location: str
+    city: str
     league: str
     phone_number: str
     main_referee: str
     level: str
     language: str
     link: str
+    sponsor_link: str
     format: str
     fraction: str
     referee: str
+    prize_pool: int
+    volume: float
 
 
 class TournamentCreate(BaseTournament):
